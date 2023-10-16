3420db730f548bdb14101074212666a0a86d718b
DeldMi <andre88444@gmail.com>
2023-10-16 14:07:22 -0300

    master

---
+++ src/app/Componetes/components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { HeaderComponent } from "./Header.componete";
+
+
+
+@NgModule({
+    declarations: [
+        HeaderComponent
+    ],
+    imports: [],
+    exports: [
+        HeaderComponent
+    ],
+    providers: [],
+    bootstrap: []
+  })
+export class ComponentModule {
+
+}
